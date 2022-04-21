3223e5639d892afd16769eac29fe981a71019fc8
rimonspi12 <rimonmpi@gmail.com>
2022-04-21 22:36:51 +0600

    ladies product added

--- src/Pages/Products/Ladie/Ladie.js
+++ src/Pages/Products/Ladie/Ladie.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import ReactStars from 'react-rating-stars-component';
+import './Ladie.css';
 
-const Ladie = ({product}) => {
-    const { name, images,price,rating,country } = product;
+const Ladie = ({ product }) => {
+    const { name, images, price, rating, country } = product;
     return (
-        <div>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" className='img-fluid' src={images} />
-                <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </div>
+        <>
+            <div className='mt-2'>
+                <div className="shadow p-3 mb-5 bg-body rounded div-height">
+                    <div className="image text-start">
+                        <div id="zoom-In">
+                            <figure>
+                                <img id="img-style" src={images} className='img-fluid' style={{ width: '100%' }} alt="" />
+                            </figure>
+                            <h5 className="fw-lighter" style={{ color: '#22789A' }}>{name}</h5>
+                            <h6 className="fw-normal" style={{ color: '#05445D' }}>${price}</h6>
+                            <h6 className="fw-normal" style={{ color: '#05445D' }}>
+                                <ReactStars
+                                    style={{ textAlign: 'center' }}
+                                    classNames='user-icon'
+                                    size={24}
+                                    isHalf={true}
+                                    edit={false}
+                                    count={Number(rating)}
+                                    value={Number(rating)}
+                                    activeColor="#ffd700"
+                                />
+                            </h6>
+                            <p className='fw-normal text-start'>{country}</p>
+                            {/* <Link to={`/order/${_id}`}> */}
+                            <button className="btn-style btn btn-primary badge rounded-pill bg-primary">shop Now</button>
+                            {/* </Link> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
