16c6f8cab8c3ad345a49c329767d33e5aa412390
rimonspi12 <rimonmpi@gmail.com>
2022-05-02 21:19:20 +0600

    all orders database table desing

--- src/Pages/DashBoard/AllOrders/AllOrders.js
+++ src/Pages/DashBoard/AllOrders/AllOrders.js
@@ -3,59 +3,59 @@ import './AllOrders.css';
 
 const AllOrders = () => {
     const [allOrders, setAllOrders] = useState([]);
+    // const [active,setActive] = useState(false);
 
     useEffect(() => {
         fetch('http://localhost:5000/allOrders')
-        .then(res => res.json())
-        .then(data => setAllOrders(data));
-    },[])
-    
-    // fetch('http://localhost:5000/allOrders')
-    // .then(res => res.json())
-    // .then(data => {
-    //     setAllOrders(data)
-    //     console.log(data);
-    // })
+            .then(res => res.json())
+            .then(data => {
+                setAllOrders(data)
+                console.log(data);
+            });
+    }, [])
+
+
 
     return (
         <div>
             <h2>This is All Users Collections: {allOrders.length}</h2>
-            <div className="table-responsive">
-                <table className="table" id="customer">
-                    <thead>
-                        <tr>
-                            <th>IMG</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Price</th>
-                            <th>Location</th>
-                            <th>Phone</th>
-                            <th>Quantity</th>
-                            <th>status</th>
-                            <th>Decision</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                         allOrders.map((order,index) => <>
+            <div style={{ marginLeft: '50px' }} className="mt-3">
+                <div className='table-responsive'>
+                    <table id='customer'>
+                        <thead>
                             <tr>
-                                 <td key={order._id}><img style={{width:'100%', height:'50px'}} src={order.productImg} alt="" /></td>
-                                 <td>{order.name}</td>
-                                 <td>{order._id}</td>
-                                 <td>{order.email}</td>
-                                 <td>{order.price}</td>
-                                 <td>{order.Present_Address}</td>
-                                 <td>{order.mobile}</td>
-                                 <td>{order.quantity}</td>
-                                 <td>Pending</td>
-                                  {/* <button onClick={() => handleCancelButton(order._id)} className='btn btn-danger my-2 fw-bold ms-3'><MdDelete size={25} />Cancel</button> */}
-                                 <button type="button" className="btn btn-primary">Deletion</button>
-
+                                <th>IMG</th>
+                                <th>ProductName</th>
+                                <th>price</th>
+                                <th>quantity</th>
+                                <th>userName</th>
+                                <th>Email</th>
+                                <th>Location </th>
+                                <th>Phone</th>
+                                <th>Status</th>
+                                <th>Decision</th>
                             </tr>
-                         </>)
-                        }
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {
+                                allOrders.map((order, index) => <>
+                                    <tr>
+                                        <td><img src={order.productImg} style={{ height: '50px', width: '100%' }} alt="product_img" /></td>
+                                        <td>{order.productName}</td>
+                                        <td>{order.price}</td>
+                                        <td>{order.quantity}</td>
+                                        <td>{order.name}</td>
+                                        <td>{order.email}</td>
+                                        <td>{order.Present_Address}</td>
+                                        <td>{order.mobile}</td>
+                                        <td className='text-center text-danger'>pending</td>
+                                       <button className='btn btn-danger'>cancel</button>
+                                    </tr>
+                                </>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
