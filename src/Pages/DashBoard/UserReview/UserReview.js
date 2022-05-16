b37aa1731824d9e829647940b0e7982d439fb718
rimonspi12 <rimonmpi@gmail.com>
2022-05-16 12:19:10 +0600

    review section added

--- src/Pages/DashBoard/UserReview/UserReview.js
+++ src/Pages/DashBoard/UserReview/UserReview.js
@@ -3,7 +3,7 @@ import ReactStars from "react-rating-stars-component";
 import useAuth from '../../../hooks/useAuth';
 
 const UserReview = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
     const [rating, setRating] = useState(0);
     const ratingChanged = (newRating) => {
         console.log(newRating);
@@ -20,7 +20,7 @@ const UserReview = () => {
                 <div className="col-sm-12 col-md-4 mt-3">
                     <div className='d-inline'>
                         <ReactStars
-                            style={{display:'inline-block'}}
+                            style={{ display: 'inline-block' }}
                             count={5}
                             onChange={ratingChanged}
                             size={24}
@@ -32,8 +32,17 @@ const UserReview = () => {
                         />
                     </div>
                     <div className='d-inline'>
-                        <h5 className='fw-5'>Your rating: {rating}</h5>
+                        <h5 className='fw-5 mb-2'>Your rating: {rating}</h5>
                     </div>
+                </div>
+                <div className='w-75'>
+                    <input type="text" className="mt-2 form-control" placeholder='Your Address' />                   
+                </div>
+                <div className='w-75'>
+                    <textarea type="text" className="mt-2 form-control" placeholder='write something' />
+                </div>
+                <div className='mt-2'>
+                    <button className='btn btn-primary'>submit</button>
                 </div>
             </div>
         </div>
